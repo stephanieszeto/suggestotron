033d1a06137f6e3ea224034ce72e9531a6989f83
Stephanie Szeto <stephaniexszeto@gmail.com>
2012-05-05 13:39:21 -0700

    adding additional files

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
   has_many :votes
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120114202144) do
+ActiveRecord::Schema.define(:version => 20120428220308) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "topic_id"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "topics", :force => true do |t|
     t.string   "title"
